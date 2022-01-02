2f50b10d6bd32afa59c8c81417d408adb6163811
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-01-01 23:54:52 -0300

    Create omni_world.sdf

--- omnibot_description/launch/spawn_ignition.launch.py
+++ omnibot_description/launch/spawn_ignition.launch.py
@@ -10,7 +10,9 @@ import os
 def generate_launch_description():
 
   pkg_share = FindPackageShare(package='omnibot_description').find('omnibot_description')
-  default_model_path = os.path.join(pkg_share, 'urdf/omnibot.urdf.xacro')
+  default_world_path = os.path.join(pkg_share, 'worlds/omni_world.sdf')
+  default_model_sdf_path = os.path.join(pkg_share, 'urdf/omnibot.sdf')
+  default_model_urdf_path = os.path.join(pkg_share, 'urdf/omnibot.urdf.xacro')
   default_rviz_config_path = os.path.join(pkg_share, 'rviz/config.rviz')
 
   pkg_ros_ign_gazebo = FindPackageShare('ros_ign_gazebo').find('ros_ign_gazebo')
@@ -18,15 +20,18 @@ def generate_launch_description():
   ignition = IncludeLaunchDescription(
              PythonLaunchDescriptionSource(
                os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py')),
-             launch_arguments={'ign_args': '-r empty.sdf'}.items(),
+            #  launch_arguments={'ign_args': '-r empty.sdf'}.items(),
+             launch_arguments={'ign_args': ['-r ', LaunchConfiguration('world')]}.items(),
   )
 
   spawn_entity = Node(
     package='ros_ign_gazebo',
     executable='create',
     arguments=['-name', 'omnibot',
-               '-topic', 'robot_description',
-               '-z', '0.25'],
+              #  '-topic', 'robot_description',
+               '-z', '0.25',
+               '-file', default_model_sdf_path
+              ],
     output='screen'
   )
 
@@ -39,9 +44,11 @@ def generate_launch_description():
   robot_state_publisher = Node(
     package='robot_state_publisher',
     executable='robot_state_publisher',
-    parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')]),
-                 'use_sim_time': LaunchConfiguration('use_sim_time')}],
-    output='both'
+    parameters=[{'robot_description': Command(['xacro ', default_model_urdf_path]),
+                 'use_sim_time': LaunchConfiguration('use_sim_time'),
+                }],
+    # parameters=[{'robot_description': LaunchConfiguration('model'),
+    # output='both'
   )
   
   rviz_node = Node(
@@ -52,29 +59,28 @@ def generate_launch_description():
     arguments=['-d', LaunchConfiguration('rvizconfig')]
   )
 
-  # Ign - ROS Bridge
   ign_ros_bridge = Node(
     package='ros_ign_bridge',
     executable='parameter_bridge',
     arguments=[
-            # Clock (IGN -> ROS2)
             '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
-            # Joint states (IGN -> ROS2)
-            '/world/empty/model/omnibot/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model',
-            # IMU (IGN -> ROS2)
+            '/world/omni_world/model/omnibot/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model',
             '/imu@sensor_msgs/msg/Imu@ignition.msgs.IMU',
-            # LIDAR (IGN -> ROS2)
-            '/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan'
+            '/lidar@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+            '/camera@sensor_msgs/msg/Image@ignition.msgs.Image',
+            '/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo'
             ],
     remappings=[
-        ('/world/empty/model/omnibot/joint_state', 'joint_states'),
+        ('/world/omni_world/model/omnibot/joint_state', 'joint_states'),
     ],
     output='screen'
   )
 
   return launch.LaunchDescription([
-    launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                         description='Absolute path to robot urdf file'),
+    launch.actions.DeclareLaunchArgument(name='world', default_value=default_world_path,
+                                         description='Absolute path to world sdf file'),
+    # launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                        #  description='Absolute path to robot urdf file'),
     launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                          description='Absolute path to rviz config file'),
     launch.actions.DeclareLaunchArgument(name='use_sim_time',default_value='true',
