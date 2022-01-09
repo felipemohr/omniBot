2875ec49da88b7511a1b70c0098f635d86111aa1
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-01-09 03:04:23 -0300

    Update plugin Configure

--- omnibot_ignition/plugin/omni_drive/OmniDrive.cc
+++ omnibot_ignition/plugin/omni_drive/OmniDrive.cc
@@ -4,20 +4,6 @@ using namespace ignition;
 using namespace gazebo;
 using namespace omni_drive;
 
-/** @brief Velocity command. **/
-struct Commands
-{
-  /** @brief Linear velocity in X-axis. **/
-  double lin_x;
-
-  /** @brief Linear velocity in Y-axis. **/
-  double lin_y;
-
-  /** @brief Angular velocity in Z-axis. **/
-  double ang;
-
-  Commands() : lin_x(0.0), lin_y(0.0), ang(0.0) {}
-};
 
 //////////////////////////////////////////////////
 OmniDrive::OmniDrive()
@@ -31,7 +17,128 @@ void OmniDrive::Configure(const Entity &_entity,
     EntityComponentManager &_ecm,
     EventManager &/*_eventMgr*/)
 {
- 
+  this->dataPtr->model = Model(_entity);
+
+  // Get the canonical link
+  std::vector<Entity> links = _ecm.ChildrenByComponents(
+    this->dataPtr->model.Entity(), components::CanonicalLink());
+  if (!links.empty())
+    this->dataPtr->canonicalLink = Link(links[0]);
+
+  if (!this->dataPtr->model.Valid(_ecm))
+  {
+    ignerr << "OmniDrive plugin should be attached to a model entity. "
+           << "Failed to initialize." << std::endl;
+    return;
+  }
+
+  // Ugly, but needed because the sdf::Element::GetElement is not a const
+  // function and _sdf is a const shared pointer to a const sdf::Element.
+  auto ptr = const_cast<sdf::Element *>(_sdf.get());
+
+  // Get params from SDF
+  sdf::ElementPtr sdfElem;
+  sdfElem = ptr->GetElement("front_left_joint");
+  this->dataPtr->frontLeftJointName = sdfElem->Get<std::string>();
+
+  sdfElem = ptr->GetElement("rear_left_joint");
+  this->dataPtr->rearLeftJointName = sdfElem->Get<std::string>();
+
+  sdfElem = ptr->GetElement("front_right_joint");
+  this->dataPtr->frontRightJointName = sdfElem->Get<std::string>();
+
+  sdfElem = ptr->GetElement("rear_right_joint");
+  this->dataPtr->rearRightJointName = sdfElem->Get<std::string>();
+
+  this->dataPtr->wheelRightLeftSeparation = _sdf->Get<double>("wheel_right_left_separation",
+      this->dataPtr->wheelRightLeftSeparation).first;
+  this->dataPtr->wheelFrontRearSeparation = _sdf->Get<double>("wheel_front_rear_separation",
+      this->dataPtr->wheelFrontRearSeparation).first;
+  this->dataPtr->wheelRadius = _sdf->Get<double>("wheel_radius",
+      this->dataPtr->wheelRadius).first;
+
+  // Instantiate the speed limiters.
+  this->dataPtr->limiterLin = std::make_unique<ignition::math::SpeedLimiter>();
+  this->dataPtr->limiterAng = std::make_unique<ignition::math::SpeedLimiter>();
+
+  // Parse speed limiter parameters.
+  if (_sdf->HasElement("min_velocity"))
+  {
+    const double minVel = _sdf->Get<double>("min_velocity");
+    this->dataPtr->limiterLin->SetMinVelocity(minVel);
+    this->dataPtr->limiterAng->SetMinVelocity(minVel);
+  }
+  if (_sdf->HasElement("max_velocity"))
+  {
+    const double maxVel = _sdf->Get<double>("max_velocity");
+    this->dataPtr->limiterLin->SetMaxVelocity(maxVel);
+    this->dataPtr->limiterAng->SetMaxVelocity(maxVel);
+  }
+  if (_sdf->HasElement("min_acceleration"))
+  {
+    const double minAccel = _sdf->Get<double>("min_acceleration");
+    this->dataPtr->limiterLin->SetMinAcceleration(minAccel);
+    this->dataPtr->limiterAng->SetMinAcceleration(minAccel);
+  }
+  if (_sdf->HasElement("max_acceleration"))
+  {
+    const double maxAccel = _sdf->Get<double>("max_acceleration");
+    this->dataPtr->limiterLin->SetMaxAcceleration(maxAccel);
+    this->dataPtr->limiterAng->SetMaxAcceleration(maxAccel);
+  }
+  if (_sdf->HasElement("min_jerk"))
+  {
+    const double minJerk = _sdf->Get<double>("min_jerk");
+    this->dataPtr->limiterLin->SetMinJerk(minJerk);
+    this->dataPtr->limiterAng->SetMinJerk(minJerk);
+  }
+  if (_sdf->HasElement("max_jerk"))
+  {
+    const double maxJerk = _sdf->Get<double>("max_jerk");
+    this->dataPtr->limiterLin->SetMaxJerk(maxJerk);
+    this->dataPtr->limiterAng->SetMaxJerk(maxJerk);
+  }
+
+  // Subscribe to commands
+  std::vector<std::string> topics;
+  if (_sdf->HasElement("topic"))
+  {
+    topics.push_back(_sdf->Get<std::string>("topic"));
+  }
+  topics.push_back("/model/" + this->dataPtr->model.Name(_ecm) + "/cmd_vel");
+  auto topic = validTopic(topics);
+
+  this->dataPtr->node.Subscribe(topic, &OmniDrivePrivate::OnCmdVel,
+      this->dataPtr.get());
+
+  // Subscribe to enable/disable
+  std::vector<std::string> enableTopics;
+  enableTopics.push_back(
+    "/model/" + this->dataPtr->model.Name(_ecm) + "/enable");
+  auto enableTopic = validTopic(enableTopics);
+
+  if (!enableTopic.empty())
+  {
+    this->dataPtr->node.Subscribe(enableTopic, &OmniDrivePrivate::OnEnable,
+        this->dataPtr.get());
+  }
+  this->dataPtr->enabled = true;
+
+  std::string tfTopic{"/model/" + this->dataPtr->model.Name(_ecm) +
+    "/tf"};
+  if (_sdf->HasElement("tf_topic"))
+    tfTopic = _sdf->Get<std::string>("tf_topic");
+  this->dataPtr->tfPub = this->dataPtr->node.Advertise<msgs::Pose_V>(
+      tfTopic);
+
+  if (_sdf->HasElement("frame_id"))
+    this->dataPtr->sdfFrameId = _sdf->Get<std::string>("frame_id");
+
+  if (_sdf->HasElement("child_frame_id"))
+    this->dataPtr->sdfChildFrameId = _sdf->Get<std::string>("child_frame_id");
+
+  ignmsg << "OmniDrive subscribing to twist messages on [" << topic << "]"
+         << std::endl;
 }
 
 //////////////////////////////////////////////////
@@ -40,6 +147,14 @@ void OmniDrive::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
 {
   IGN_PROFILE("OmniDrive::PreUpdate");
 
+  std::string msg = "PreUpdate! Simulation is ";
+  if (!_info.paused)
+    msg += "not ";
+  msg += "paused.";
+
+  ignmsg << msg << std::endl;
+  // std::cout << msg << std::endl;
+
 }
 
 //////////////////////////////////////////////////
@@ -47,6 +162,14 @@ void OmniDrive::PostUpdate(const UpdateInfo &_info,
     const EntityComponentManager &_ecm)
 {
   IGN_PROFILE("OmniDrive::PostUpdate");
+
+  std::string msg = "PosUpdate! Simulation is ";
+  if (!_info.paused)
+    msg += "not ";
+  msg += "paused.";
+
+  // std::cout << msg << std::endl;
+
   // Nothing left to do if paused.
   if (_info.paused)
     return;
--- omnibot_ignition/plugin/omni_drive/OmniDrive.hh
+++ omnibot_ignition/plugin/omni_drive/OmniDrive.hh
@@ -26,6 +26,22 @@
 #include "ignition/gazebo/Model.hh"
 #include "ignition/gazebo/Util.hh"
 
+/** @brief Velocity command. **/
+struct Commands
+{
+  /** @brief Linear velocity in X-axis. **/
+  double lin_x;
+
+  /** @brief Linear velocity in Y-axis. **/
+  double lin_y;
+
+  /** @brief Angular velocity in Z-axis. **/
+  double ang;
+
+  Commands() : lin_x(0.0), lin_y(0.0), ang(0.0) {}
+};
+
+
 namespace ignition
 {
 namespace gazebo
@@ -37,76 +53,114 @@ namespace omni_drive
 
   class OmniDrivePrivate
   {
-    /**
-     * @brief Callback for velocity subscription.
-     * 
-     * @param _msg Velocity message.
-     */
-    void OnCmdVel(const ignition::msgs::Twist &_msg);
+    public:
+      /**
+       * @brief Callback for velocity subscription.
+       * 
+       * @param _msg Velocity message.
+       */
+      void OnCmdVel(const ignition::msgs::Twist &_msg);
 
-    /**
-     * @brief Callback for enable/disable subscription.
-     * 
-     * @param _msg Boolean message.
-     */
-    void OnEnable(const ignition::msgs::Boolean &_msg);
+      /**
+       * @brief Callback for enable/disable subscription.
+       * 
+       * @param _msg Boolean message.
+       */
+      void OnEnable(const ignition::msgs::Boolean &_msg);
 
-    /**
-     * @brief Update linear and angular velocities.
-     * 
-     * @param _info System update information.
-     * @param _ecm  The EntityComponentManager of the given simulation instance.
-     */
-    void UpdateVelocity(const ignition::gazebo::UpdateInfo &_info,
-                        const ignition::gazebo::EntityComponentManager &_ecm);
+      /**
+       * @brief Update linear and angular velocities.
+       * 
+       * @param _info System update information.
+       * @param _ecm  The EntityComponentManager of the given simulation instance.
+       */
+      void UpdateVelocity(const ignition::gazebo::UpdateInfo &_info,
+                          const ignition::gazebo::EntityComponentManager &_ecm);
 
-    /** @brief Ignition communication node **/
-    transport::Node node;
+      /** @brief Ignition communication node **/
+      transport::Node node;
 
-    /** @brief Entity of the front left joint **/
-    Entity frontLeftJoint;
+      /** @brief Entity of the front left joint **/
+      Entity frontLeftJoint;
 
-    /** @brief Entity of the back left joint **/
-    Entity backLeftJoint;
+      /** @brief Entity of the rear left joint **/
+      Entity rearLeftJoint;
 
-    /** @brief Entity of the front right joint **/
-    Entity frontRightJoint;
+      /** @brief Entity of the front right joint **/
+      Entity frontRightJoint;
 
-    /** @brief Entity of the back right joint **/
-    Entity backRightJoint;
+      /** @brief Entity of the rear right joint **/
+      Entity rearRightJoint;
 
-    /** @brief Name of the front left joint **/
-    std::string frontLeftJointName;
+      /** @brief Name of the front left joint **/
+      std::string frontLeftJointName;
 
-    /** @brief Name of the back left joint **/
-    std::string backLeftJointName;
+      /** @brief Name of the rear left joint **/
+      std::string rearLeftJointName;
 
-    /** @brief Name of the front right joint **/
-    std::string frontRightJointName;
+      /** @brief Name of the front right joint **/
+      std::string frontRightJointName;
 
-    /** @brief Name of the back right joint **/
-    std::string backRightJointName;
+      /** @brief Name of the rear right joint **/
+      std::string rearRightJointName;
 
-    /** @brief Calculated speed of front left joint **/
-    double frontLeftJointSpeed{0};
+      /** @brief Calculated speed of front left joint **/
+      double frontLeftJointSpeed{0};
 
-    /** @brief Calculated speed of back left joint **/
-    double backLeftJointSpeed{0};
+      /** @brief Calculated speed of rear left joint **/
+      double rearLeftJointSpeed{0};
 
-    /** @brief Calculated speed of front right joint **/
-    double frontRightJointSpeed{0};
+      /** @brief Calculated speed of front right joint **/
+      double frontRightJointSpeed{0};
 
-    /** @brief Calculated speed of back right joint **/
-    double backRightJointSpeed{0};
+      /** @brief Calculated speed of rear right joint **/
+      double rearRightJointSpeed{0};
 
-    /** @brief Distance between right and left wheels **/
-    double wheelRightLeftSeparation{1.0};
+      /** @brief Distance between right and left wheels **/
+      double wheelRightLeftSeparation{1.0};
 
-    /** @brief Distance between front and back wheels **/
-    double wheelFrontBackSeparation{1.0};
+      /** @brief Distance between front and rear wheels **/
+      double wheelFrontRearSeparation{1.0};
 
-    /** @brief Wheel radius **/
-    double wheelRadius{0.2};
+      /** @brief Wheel radius **/
+      double wheelRadius{0.2};
+
+      /** @brief Model interface **/
+      Model model{kNullEntity};
+
+      /** @brief The model's canonical link **/
+      Link canonicalLink{kNullEntity};
+
+      /** @brief Omni drive tf message publisher **/
+      transport::Node::Publisher tfPub;
+
+      /** @brief Linear velocity limiter **/
+      std::unique_ptr<ignition::math::SpeedLimiter> limiterLin;
+
+      /** @brief Angular velocity limiter **/
+      std::unique_ptr<ignition::math::SpeedLimiter> limiterAng;
+
+      /** @brief Previous control command **/
+      Commands last0Cmd;
+
+      /** @brief Previous control command to last0Cmd **/
+      Commands last1Cmd;
+
+      /** @brief Last target velocity requested **/
+      msgs::Twist targetVel;
+
+      /** @brief Enable/disable state of the controller **/
+      bool enabled;
+
+      /** @brief A muterx to protect the target velocity command **/
+      std::mutex mutex;
+
+      /** @brief frame_id from sdf **/
+      std::string sdfFrameId;
+
+      /** @brief child_frame_id from sdf **/
+      std::string sdfChildFrameId;
+
   };
 
 
@@ -118,34 +172,36 @@ namespace omni_drive
         public ISystemPreUpdate,
         public ISystemPostUpdate
   {
-    /** @brief Constructor **/
-    public: OmniDrive();
+    public: 
+      /** @brief Constructor **/
+      OmniDrive();
 
-    /** @brief Destructor **/
-    public: ~OmniDrive() override = default;
+      /** @brief Destructor **/
+      ~OmniDrive() override = default;
 
-    /** Documentation inherited **/
-    public: void Configure(const Entity &_entity,
-                           const std::shared_ptr<const sdf::Element> &_sdf,
-                           EntityComponentManager &_ecm,
-                           EventManager &_eventMgr) override;
+      /** Documentation inherited **/
+      void Configure(const Entity &_entity,
+                     const std::shared_ptr<const sdf::Element> &_sdf,
+                     EntityComponentManager &_ecm,
+                     EventManager &_eventMgr) override;
 
-    /** Documentation inherited **/
-    public: void PreUpdate(
-                const ignition::gazebo::UpdateInfo &_info,
-                ignition::gazebo::EntityComponentManager &_ecm) override;
+      /** Documentation inherited **/
+      void PreUpdate(const ignition::gazebo::UpdateInfo &_info,
+                     ignition::gazebo::EntityComponentManager &_ecm) override;
 
-    /** Documentation inherited **/
-    public: void PostUpdate(
-                const UpdateInfo &_info,
-                const EntityComponentManager &_ecm) override;
+      /** Documentation inherited **/
+      void PostUpdate(const UpdateInfo &_info,
+                      const EntityComponentManager &_ecm) override;
 
-    /** Private data pointer **/
-    private: std::unique_ptr<OmniDrivePrivate> dataPtr;
+    private: 
+      /** Private data pointer **/
+      std::unique_ptr<OmniDrivePrivate> dataPtr;
   };
-  }
-}
-}
-}
+  
+}// namespace omni_drive
+
+} // namespace IGNITION_GAZEBO_VERSION_NAMESPACE
+} // namespace gazebo
+} // namespace ignition
 
 #endif
