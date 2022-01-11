5e4d76ec00c72987c8d1458171deee2958f40c60
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-01-11 11:28:42 -0300

    Finish KeyboardInterface

--- keyboard_interface/scripts/KeyboardInterface.py
+++ keyboard_interface/scripts/KeyboardInterface.py
@@ -1,38 +1,63 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from pynput.keyboard import Listener
 
 from keyboard_interface.msg import Keys
+from keyboard_interface.msg import KeyEvent
+
+try:
+  from pynput.keyboard import Listener
+except ImportError as e:
+  import pip
+  print("pynput package is required. Installing...")
+  pip.main(['install', 'pynput'])
+  from pynput.keyboard import Listener
+
 
 class KeyboardInterface(Node):
 
   def __init__(self):
     super().__init__('keyboard_interface')
-    self.publisher_ = self.create_publisher(Keys, '/keyboard', 30)
-    timer_period = 0.1
+    self.keys_publisher_ = self.create_publisher(Keys, '/keys', 10)
+    self.key_event_publisher_ = self.create_publisher(KeyEvent, '/key_event', 10)
+
+    self.sticky_buttons = self.declare_parameter('sticky_buttons', False).get_parameter_value().bool_value
+    self.keys_publish_rate = self.declare_parameter('keys_publish_rate', 10.0).get_parameter_value().double_value
+
+    # self.sticky_buttons = self.get_parameter('sticky_buttons').get_parameter_value().bool_value
+    # self.keys_publish_rate = self.get_parameter('keys_publish_rate').get_parameter_value().double_value
+
     self.keys_msg = Keys()
-    self.timer = self.create_timer(timer_period, self.timer_callback)
+    self.timer = self.create_timer(1/self.keys_publish_rate, self.timerCallback)
     listener = Listener(on_press=self.onPress, on_release=self.onRelease)
     listener.start()
   
-  def timer_callback(self):
-    self.publisher_.publish(self.keys_msg)
+  def timerCallback(self):
+    self.keys_publisher_.publish(self.keys_msg)
 
   def onRelease(self, key):
     key = str(key).upper()
-    if key in self.keys_msg.pressed_keys:
-      self.keys_msg.pressed_keys.remove(key)
-      self.publisher_.publish(self.keys_msg)
+    key_event_msg = KeyEvent()
+    key_event_msg.event = KeyEvent.RELEASED
+    key_event_msg.key = key
+    self.key_event_publisher_.publish(key_event_msg)
+
+    if not self.sticky_buttons:
+      if key in self.keys_msg.pressed_keys:
+        self.keys_msg.pressed_keys.remove(key)
+
 
   def onPress(self, key):
     key = str(key).upper()
+    key_event_msg = KeyEvent()
+    key_event_msg.event = KeyEvent.PRESSED
+    key_event_msg.key = key
+    self.key_event_publisher_.publish(key_event_msg)
+
     if key not in self.keys_msg.pressed_keys:
       self.keys_msg.pressed_keys.append(key)
-      print(key)
-      self.publisher_.publish(self.keys_msg)
-
-
+    elif self.sticky_buttons:
+      self.keys_msg.pressed_keys.remove(key)
 
 
 def main(args=None):
