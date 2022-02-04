f814b587c4ddd531f2adddc1402fde5a3bf21e58
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-02-04 00:51:29 -0300

    Update world models position

--- install/keyboard_interface/share/keyboard_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(keyboard_interface_IDL_FILES "msg/Keys.idl;msg/KeyEvent.idl")
-set(keyboard_interface_INTERFACE_FILES "msg/Keys.msg;msg/KeyEvent.msg")
